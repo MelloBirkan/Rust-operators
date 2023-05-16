596dac7c742c95f74e116a2fb8db3659ffba6f9f
marcellogonzattobirkan <mgbirkan@icloud.com>
2023-05-16 20:57:10 -0300

    Aula de operadores

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
+#![allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
     let sum = 3 +4;
+    let modulo = 40 % 26;
+    println!("sum: {sum}, modulo: {modulo}");
+
+    // Exponencial
+    let quadrado = i64::pow(8, 2);
+    println!("quadrado: {quadrado}");
+
+    // Bitwise
+    let and = 0b0011 & 0b0101;
+    let or = 0b0011 | 0b0101;
+    let xor = 0b0011 ^ 0b0101;
+    let not = !0b0011;
+    let shift_left = 0b0011 << 1;
+    let shift_right = 0b0011 >> 1;
+    println!("and: {and}, or: {or}, xor: {xor}, not: {not}, shift_left: {shift_left}, shift_right: {shift_right}");
 }
